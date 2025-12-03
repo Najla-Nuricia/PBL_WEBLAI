b9dbbd37682384ec7d502b0bf529807f28c5d049
ezreals-ares <nawaf303@gmail.com>
2025-12-03 23:38:15 +0700

    refactor: back to cdn module

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -2,26 +2,27 @@
 </div> <!-- End Main Content -->
 
 <!-- jQuery -->
-<script src="../node_modules/jquery/dist/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
 <!-- Bootstrap 5 JS -->
-<script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <!-- DataTables JS -->
 <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
 
 <!-- SweetAlert2 -->
-<script src="../node_modules/sweetalert2/dist/sweetalert2.all.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <!-- Select2 JS -->
-<script src="../node_modules/select2/dist/js/select2.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 <!-- Quill JS -->
-<script src="../node_modules/quill/dist/quill.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/quill@2.0.0-rc.2/dist/quill.js"></script>
 
 <!-- flatpickr JS -->
-<script src="../node_modules/flatpickr/dist/flatpickr.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+<script src="https://npmcdn.com/flatpickr/dist/l10n/id.js"></script>
 
 <!-- Custom JS -->
 <script src="../assets/js/public.js"></script>
