ba3dacd5e9cd2d5bcf01049bbf934b307fa546eb
ezreals-ares <nawaf303@gmail.com>
2025-12-03 13:03:39 +0700

    feat: menambahkan path dependensi quilljs

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -17,7 +17,6 @@
 <!-- Select2 JS -->
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
-
 <!-- Quill JS -->
 <script src="../node_modules/quill/dist/quill.js"></script>
 
